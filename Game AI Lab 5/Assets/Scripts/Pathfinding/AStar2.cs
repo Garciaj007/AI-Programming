e8d38a714202afb876ab2350a1fd19aba08c6977
Garciaj007 <jurielgarcia2010@gmail.com>
2019-11-07 18:24:37 -0500

    Lab 5 Integration

---
+++ Game AI Lab 5/Assets/Scripts/Pathfinding/AStar2.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pathfinding {
+	public class AStar2 {
+		public Tilemap map;
+
+		List<Node> visited = new List<Node>();
+		List<Node> unvisited = new List<Node>();
+
+		Dictionary<Node, Node> predecessorDict = new Dictionary<Node, Node>();
+		Dictionary<Node, float> fDistanceDict = new Dictionary<Node, float>();
+		Dictionary<Node, float> gDistanceDict = new Dictionary<Node, float>();
+
+		public void Init(Tilemap tileMap)
+		{
+			map = tileMap;
+		}
+		
+		public List<Node> Search(Node start, Node goal)
+		{
+			// 1. dist[s] = 0
+			// 2. set all other distances to infinity
+            List<Node> nodes = map.GetAllNodes();
+            foreach (Node node in nodes)
+			{
+                fDistanceDict[node] = float.MaxValue;
+                gDistanceDict[node] = float.MaxValue;
+			}
+            fDistanceDict[start] = 0;
+            gDistanceDict[start] = 0;
+
+			// 3. Initialize S(visited) and Q(unvisited)
+			//    S, the set of visited nodes is initially empty
+			//    Q, the queue initially conatains all nodes
+        	// To do: Initialize visited and unvisited
+        
+			
+			predecessorDict.Clear(); // to generate the result path
+			
+			while (unvisited.Count > 0)
+			{
+				// 4. select element of Q with the minimum distance
+            	// To do: Get a closest node from the unvisited list
+            	// Node u = ?
+            	Node u = null;
+
+				// Check if the node u is the goal.            
+				if (u == goal) break;
+							
+				// 5. add u to list of S(visited)            
+            	// To do: add u to the visited list
+				
+				foreach(Node v in map.GetNeighbors(u))
+				{
+					if (visited.Contains(v))
+						continue;
+
+					// 6. If new shortest path found then set new value of shortest path                
+	                // To do: update fDistanceDict[v], gDistanceDict[v] and predecessorDict[v]
+	                
+				}
+			}
+
+			List<Node> path = new List<Node>();
+
+			// Generate the shortest path
+			path.Add(goal);
+			Node p = predecessorDict[goal];
+			
+			while (p != start)
+			{
+				path.Add(p);            
+				p = predecessorDict[p];        
+			}
+
+			path.Reverse();
+			return path;
+		}
+		
+		Node GetClosestFromUnvisited()
+		{
+			float shortest = float.MaxValue;
+			Node shortestNode = null;
+			foreach (var node in unvisited)
+			{
+				if (shortest > fDistanceDict[node])
+				{
+					shortest = fDistanceDict[node];
+					shortestNode = node;
+				}
+			}
+			
+			unvisited.Remove(shortestNode);
+			return shortestNode;
+		}   
+	}
+}
---
+++ Game AI Lab 5/Assets/Scripts/Pathfinding/Character.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pathfinding {
+	public class Character : MonoBehaviour {
+		public Tilemap map;
+		AStar2 aStarPathfinder = new AStar2();
+		public enum State { MOVE, IDLE };
+		State state;
+
+		// Use this for initialization
+		void Start () {
+			state = State.IDLE;
+			aStarPathfinder.Init(map);	    	
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			// handle mouse input
+			if (state == State.IDLE && Input.GetMouseButtonDown(0))
+			{
+				state = State.MOVE;
+				Vector3 pos = Input.mousePosition;
+				pos = Camera.main.ScreenToWorldPoint(pos);
+				
+				Node targetNode = map.GetNode(Mathf.FloorToInt(pos.x + 0.5f), Mathf.FloorToInt(pos.y + 0.5f));
+
+				if (targetNode.tileType == Node.TileType.GRASS)
+				{
+					StartCoroutine(SearchPathAndMove(targetNode));
+				}
+				else
+				{
+					state = State.IDLE;
+					Debug.Log("WALL - can't move to here");
+				}
+				
+			}
+		}
+
+		IEnumerator SearchPathAndMove(Node target)
+		{
+			// To do: Find out a start node. The start node is the node where the character stands
+			// Node start = ?
+
+
+			// To do: Get the shortest path between start and target using astar algorithm
+			// List<Node> path = ?
+
+
+
+			yield return null;
+			// To do: move the character using the position info in the shortest path
+			// you need to use "yield return new WaitForSeconds(0.5f);" to make a delay between each movement
+			// Refer to https://docs.unity3d.com/Manual/Coroutines.html
+			
+
+
+
+
+
+
+
+
+			// set state to IDLE in order to enable next movement
+			state = State.IDLE;
+		}
+	}
+}
