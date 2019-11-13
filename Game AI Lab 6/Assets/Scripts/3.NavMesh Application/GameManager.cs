fffd784feaa69ea873bfe14d6be65ae178c7673c
Juriel Garcia <jurielgarcia2010@gmail.com>
2019-11-13 13:09:22 -0500

    Game AI Lab 6 Integration

---
+++ Game AI Lab 6/Assets/Scripts/3.NavMesh Application/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject[] spawningPoints;
+    public GameObject alien;
+    public GameObject player;
+	// Use this for initialization
+	IEnumerator Start () {
+		while (true)
+        {
+            // To do : Select one of the spawning points(spawningPoints) randomly
+            // and then instantiate an alien object
+            // Don't forget to set the alien's target to player
+            
+            yield return new WaitForSeconds(5f);
+        }
+	}
+}
